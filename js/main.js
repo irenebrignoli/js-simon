d35c5b79d51867bc0ae06ea24102a042f11e789b
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-07 15:15:00 +0100

    funzione countdown inserita

--- js/main.js
+++ js/main.js
@@ -9,3 +9,68 @@ Consigli del giorno:
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 */
 
+const numbersDom = document.getElementById('numbers');
+const countdownDom = document.getElementById('countdown');
+const endTimeDom = document.getElementById('end-time');
+let arrayNumbers = [];
+
+
+for(let i = 1; i<=5; i++){
+
+  let number = generateUniqueRandomNumber(arrayNumbers, 1, 50);
+  arrayNumbers.push(number);
+  
+}
+
+console.log(arrayNumbers);
+numbersDom.innerHTML = arrayNumbers;
+
+let seconds = 31;
+
+const countdown = setInterval(runCountdown, 1000);
+
+console.log('fuori '+ countdown);
+
+
+
+//FUNZIONI
+
+
+//conto alla rovescia
+function runCountdown(){
+
+  seconds--;
+
+  if (seconds <= 0){
+    clearInterval(countdown);
+    endTimeDom.innerHTML = 'Tempo finito!';
+  }
+
+ countdownDom.innerHTML = seconds;
+}
+
+
+//genera numero casuale che non si ripete
+function generateUniqueRandomNumber(array, min, max){
+
+  let validNumber = false;
+  let randomNumber;
+
+  while (!validNumber){
+    randomNumber = generateRandomNumber(min, max);
+    if (!array.includes(randomNumber)){
+      validNumber = true;
+    }
+  }
+
+  return randomNumber;
+}
+
+
+//genera numero casuale
+function generateRandomNumber(min, max){
+
+  const number = Math.floor(Math.random() * (max - min +1)) + min;
+  return number;
+}
+
